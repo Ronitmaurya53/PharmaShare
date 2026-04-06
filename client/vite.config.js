9f4e806b4a5f696e5f91d4d30cae16a91e4374cd
Ronitmaurya53 <ronitmaurya993020@gmail.com>
2026-04-06 22:40:19 +0530

    removed minute_delivery image reference

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,27 @@
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+
+// // // https://vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8080',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
 // })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -13,12 +30,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://pharmashare-backend.onrender.com',
         changeOrigin: true,
         secure: false,
       },
     },
   },
 })
-
-
