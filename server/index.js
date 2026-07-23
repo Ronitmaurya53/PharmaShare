c698643d4ace053c27ad9c9f5bf90974628daa67
Ronitmaurya53 <ronitmaurya993020@gmail.com>
2026-07-24 00:09:56 +0530

    fixed cors origin for vercel

--- server/index.js
+++ server/index.js
@@ -18,10 +18,19 @@ import chatRoute from "./route/chatRoute.js";
 
 const app = express()
 
+// app.use(cors({
+//     credentials : true,
+//     origin : process.env.FRONTEND_URL
+// }))
+
 app.use(cors({
-    credentials : true,
-    origin : process.env.FRONTEND_URL
-}))
+    origin: [
+        "http://localhost:5173", // Taki local par bhi chalta rahe
+        "https://pharma-share-pumq-5jqmyu5t3-ronitmaurya53s-projects.vercel.app" // Aapka live Vercel link
+    ],
+    credentials: true
+}));
+
 
 app.use(express.json())
 app.use(cookieParser())
